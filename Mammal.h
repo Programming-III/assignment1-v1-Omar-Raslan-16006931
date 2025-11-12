2af1840b233d671704bbecb298befedee5d21383
Omar-Raslan-16006931 <omar.raslan@student.giu-uni.de>
2025-11-12 15:10:18 +0200

    Implement Mammal class with fur color management
    Added Mammal class with attributes and methods for fur color.

--- Mammal.h
+++ Mammal.h
@@ -1,13 +1,69 @@
-#ifndef MAMMAL_H
-#define MAMMAL_H
+#include <iostream>
+#include <ostream>
+#include <set>
+#include <string>//
 
 #include "Animal.h"
+// Created by Omar on 2025-11-12.
+//
+using namespace std;
+#ifndef ASSIGNMENT_MAMMAL_H
+#define ASSIGNMENT_MAMMAL_H
 
-//define mammal class here
+
+
+class Mammal : public Animal {
+private:
+    string furColor;
+
+public:
+    Mammal() {
+        setAge(0);
+        setName("");
+        setHunger(true);
+        this->furColor="";
+    }
+
+    Mammal(string n, string fur, int a, bool h) {
+        setAge(a);
+        setName(n);
+        setHunger(h);
+        this->furColor=fur;
+    }
+
+~Mammal(){}
+
+   void setFur(string f) {
+        this->furColor=f;
+    }
+
+    string getFur() {
+        return furColor;
+    }
+
+    void display() {
+
+        cout << getName() << " (Age: " << getAge() << ", "  ;
+        if (isHungryy()){ cout << "Hungry )" << endl;}
+        else {cout << "NotHungry)" << endl;}
+    }
+
+
+
+};
 
 
 
 
 
 
-#endif
+
+
+
+
+
+
+
+
+
+#endif //ASSIGNMENT_MAMMAL_H
