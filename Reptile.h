ea594cf5f44c0cb1d0d73b096559e1ca3b65e106
Omar-Raslan-16006931 <omar.raslan@student.giu-uni.de>
2025-11-12 15:10:48 +0200

    Implement Reptile class with venomous attribute
    Added Reptile class with properties and methods.

--- Reptile.h
+++ Reptile.h
@@ -1,9 +1,58 @@
-#ifndef REPTILE_H
-#define REPTILE_H
+#include <iostream>
+#include <ostream>
+#include <set>
+#include <string>//
 
 #include "Animal.h"
+// Created by Omar on 2025-11-12.
+//
+using namespace std;//
+// Created by Omar on 2025-11-12.
+//
 
-//define reptile class here
+#ifndef ASSIGNMENT_REPTILE_H
+#define ASSIGNMENT_REPTILE_H
+
+
+
+class Reptile: public Animal {
+
+private:
+    bool isVenomous;
+    // able to produce venom or not
+
+public:
+    Reptile() {
+        setName("");
+        setAge(0);
+        setHunger(true);
+        this->isVenomous=false;
+    }
+
+    Reptile(string n, int a, bool h, bool v) {
+        setName(n);
+        setAge(a);
+        setHunger(h);
+        this->isVenomous=v;
+    }
+~Reptile(){}
+
+
+    void setVenomous(bool v) {
+        this->isVenomous=v;
+    }
+
+    bool isVenomouss() {
+        return isVenomous;
+    }
+    void display() {
+
+        cout << getName() << " (Age: " << getAge() << ", "  ;
+        if (isVenomouss()){ cout << "Venomous, " ;}
+        else {cout << "Not Venomous, " ;}
+        if (isHungryy()){ cout << "Hungry )" << endl;}
+        else {cout << "NotHungry)" << endl;}
+    }
 
 
 
@@ -13,4 +62,33 @@
 
 
 
-#endif
+
+
+
+
+
+
+
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif //ASSIGNMENT_REPTILE_H
