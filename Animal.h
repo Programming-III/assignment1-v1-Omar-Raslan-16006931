477588511e08cb4eeb5e110eb0e08f12ebec5910
Omar-Raslan-16006931 <omar.raslan@student.giu-uni.de>
2025-11-12 15:08:48 +0200

    Implement Animal class with attributes and methods
    Added a new Animal class with attributes and methods for managing animal properties.

--- Animal.h
+++ Animal.h
@@ -1,11 +1,83 @@
-#ifndef ANIMAL_H
-#define ANIMAL_H
-
-#include <string>
+#include <iostream>
+#include <ostream>
+#include <set>
+#include <string>//
+// Created by Omar on 2025-11-12.
+//
 using namespace std;
+#ifndef ASSIGNMENT_ANIMAL_H
+#define ASSIGNMENT_ANIMAL_H
 
-//define animal class here 
+
+class Animal {
+
+private:
+    string name;
+    int age;
+    bool isHungry;
+
+public:
+    Animal() {
+    this->name="";
+        this->age=0;
+        this->isHungry=false;
+    };
+    Animal(string n, int a , bool h) {
+     this->age=a;
+     this->isHungry=h;
+        this->name=n;
+
+    };
+  ~Animal(){}
+
+    void setName(string n) {
+      this->name=n;
+  }
 
 
 
-#endif
+void setAge(int a) {
+      this->age=a;
+  }
+void setHunger(bool h) {
+      this->isHungry=h;
+  }
+
+
+int getAge() {
+      return age;
+  }
+
+bool isHungryy() {
+      return isHungry;
+  };
+
+string getName() {
+    return name;
+}
+
+void display() {
+
+cout << name << " (Age: " << age << ", " ;
+    if (isHungry=true){ cout << "Hungry )" << endl;}
+    else {cout << "NotHungry)" << endl;}
+}
+
+void feed() {
+    if (isHungry) {
+        isHungry=false;
+        return;
+    }
+    else {
+       return;
+    }
+}
+
+
+
+};
+
+
+
+
+#endif //ASSIGNMENT_ANIMAL_H
