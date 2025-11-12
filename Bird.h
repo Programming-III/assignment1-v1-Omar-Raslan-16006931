e0b2985d26238229318db5764c9c148b95a0de95
Omar-Raslan-16006931 <omar.raslan@student.giu-uni.de>
2025-11-12 15:09:09 +0200

    Implement Bird class with wing span functionality
    Added Bird class with attributes and methods for wing span.

--- Bird.h
+++ Bird.h
@@ -1,9 +1,59 @@
-#ifndef BIRD_H
-#define BIRD_H
+#include <iostream>
+#include <ostream>
+#include <set>
+#include <string>//
 
 #include "Animal.h"
+// Created by Omar on 2025-11-12.
+//
+using namespace std;//
+// Created by Omar on 2025-11-12.
+//
 
-//define class bird here 
+#ifndef ASSIGNMENT_BIRD_H
+#define ASSIGNMENT_BIRD_H
+
+
+
+class Bird: public Animal {
+
+private:
+    float wingSpan;
+
+public:
+    Bird() {
+        setAge(0);
+        setHunger(true);
+        setName("");
+        this->wingSpan=0.0;
+    }
+
+    Bird(string n, int a , bool h, float w) {
+        setAge(a);
+        setHunger(h);
+        setName(n);
+        this->wingSpan=w;
+    }
+~Bird(){}
+
+
+    void setSpan(float f) {
+        this->wingSpan=f;
+    }
+ float getSpan() {
+        return wingSpan;
+    }
+
+    void display() {
+
+        cout << getName() << " (Age: " << getAge() << ", "  ;
+        if (isHungryy()){ cout << "Hungry )" << endl;}
+        else {cout << "NotHungry)" << endl;}
+    }
+
+
+
+};
 
 
 
@@ -11,4 +61,14 @@
 
 
 
-#endif
+
+
+
+
+
+
+
+
+
+
+#endif //ASSIGNMENT_BIRD_H
