fe85bd29962d795971bb15f133bc154ac0dd87ad
Omar-Raslan-16006931 <omar.raslan@student.giu-uni.de>
2025-11-12 15:11:06 +0200

    Refactor Visitor class and improve header structure

--- Visitor.h
+++ Visitor.h
@@ -1,15 +1,71 @@
-#ifndef VISITOR_H
-#define VISITOR_H
-
-#include <string>
+#include <iostream>
+#include <ostream>
+#include <set>
+#include <string>//
+// Created by Omar on 2025-11-12.
+//
 using namespace std;
-
-//define visitor class here
-
-
+#ifndef ASSIGNMENT_VISITOR_H
+#define ASSIGNMENT_VISITOR_H
 
 
 
 
+class Visitor {
 
-#endif
+private:
+    string visitorName;
+    int ticketsBought;
+
+public:
+    Visitor() {
+        this->ticketsBought=0;
+        this->visitorName="";
+    }
+
+
+    Visitor(string name, int bought) {
+        this->ticketsBought=bought;
+        this->visitorName=name;
+    }
+
+~Visitor(){}
+
+
+    void setName(string n) {
+        this->visitorName=n;
+    }
+   int getTickets() {
+        return ticketsBought;
+    }
+   void display() {
+        cout  << endl;
+        cout  << endl;
+        cout << "Visitor Info: " << endl;
+        cout << "Name: " << visitorName << endl;
+        cout << "Tickets Bought: " << ticketsBought << endl;
+
+    }
+
+
+
+
+
+
+
+
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+#endif //ASSIGNMENT_VISITOR_H
